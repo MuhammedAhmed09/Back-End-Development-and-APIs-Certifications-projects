c3a34dd3a387298a3996bc454ea3fd151954b410
B7R <muhammedahmedragab@gmail.com>
2026-09-10 03:05:32 +0300

    fist task

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,21 @@
+function isPrime(number) {
+    if(number <= 1) {
+        return false
+    }
+
+    for(let i = 2; i <= Math.sqrt(number); i++){
+        if(number % i === 0) {
+            return false
+        }
+    }
+
+    return true;
+
+}
+
+console.log(isPrime(7))
+console.log(isPrime(8))
+
+module.exports= {
+    isPrime: isPrime
+}
